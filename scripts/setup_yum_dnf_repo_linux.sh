9141616daaae1160ee3f5146e277c7d3d5117a9c
dmazur <daniil.mazur@intel.com>
2021-12-20 21:03:48 +0300

    exclude mpi repo

--- scripts/setup_yum_dnf_repo_linux.sh
+++ scripts/setup_yum_dnf_repo_linux.sh
@@ -15,13 +15,13 @@ gpgkey=https://yum.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.
 EOF
 sudo mv /tmp/oneAPI.repo /etc/yum.repos.d/
 
-tee > /tmp/mpi.repo << EOF
-[MPI]
-name=Intel(R) MPI repository
-baseurl=https://yum.repos.intel.com/mpi
-enabled=1
-gpgcheck=1
-repo_gpgcheck=1
-gpgkey=https://yum.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2019.PUB
-EOF
-sudo mv /tmp/mpi.repo /etc/yum.repos.d/
+#tee > /tmp/mpi.repo << EOF
+#[MPI]
+#name=Intel(R) MPI repository
+#baseurl=https://yum.repos.intel.com/mpi
+#enabled=1
+#gpgcheck=1
+#repo_gpgcheck=1
+#gpgkey=https://yum.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2019.PUB
+#EOF
+#sudo mv /tmp/mpi.repo /etc/yum.repos.d/
